2f31c3939b8461050b837534cd2fdb802a73d94b
MasterDev <juanpb.96@gmail.com>
2025-04-29 10:46:56 -0500

    Stretch Goal 1: Move keyboard keys array into constants file

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,7 +5,7 @@ import { WORDS } from "../../data";
 import GuessInput from "../GuessInput";
 import GuessList from "../GuessList";
 import { checkGuess } from "../../game-helpers";
-import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { KEYBOARD_KEYS, NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import Banner from "../Banner";
 import Keyboard from "../Keyboard";
 
@@ -20,40 +20,7 @@ function Game() {
     status: false,
     hasWon: "",
   });
-  const [keyboardKeys, setKeyboardKeys] = React.useState([
-    [
-      { key: "Q", status: "" },
-      { key: "W", status: "" },
-      { key: "E", status: "" },
-      { key: "R", status: "" },
-      { key: "T", status: "" },
-      { key: "Y", status: "" },
-      { key: "U", status: "" },
-      { key: "I", status: "" },
-      { key: "O", status: "" },
-      { key: "P", status: "" },
-    ],
-    [
-      { key: "A", status: "" },
-      { key: "S", status: "" },
-      { key: "D", status: "" },
-      { key: "F", status: "" },
-      { key: "G", status: "" },
-      { key: "H", status: "" },
-      { key: "J", status: "" },
-      { key: "K", status: "" },
-      { key: "L", status: "" },
-    ],
-    [
-      { key: "Z", status: "" },
-      { key: "X", status: "" },
-      { key: "C", status: "" },
-      { key: "V", status: "" },
-      { key: "B", status: "" },
-      { key: "N", status: "" },
-      { key: "M", status: "" },
-    ],
-  ]);
+  const [keyboardKeys, setKeyboardKeys] = React.useState(KEYBOARD_KEYS);
 
   function checkGameStatus(newGuess, numOfGuesses) {
     if (newGuess === answer) {
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,36 @@
 export const NUM_OF_GUESSES_ALLOWED = 6;
+
+export const KEYBOARD_KEYS = [
+  [
+    { key: "Q", status: "" },
+    { key: "W", status: "" },
+    { key: "E", status: "" },
+    { key: "R", status: "" },
+    { key: "T", status: "" },
+    { key: "Y", status: "" },
+    { key: "U", status: "" },
+    { key: "I", status: "" },
+    { key: "O", status: "" },
+    { key: "P", status: "" },
+  ],
+  [
+    { key: "A", status: "" },
+    { key: "S", status: "" },
+    { key: "D", status: "" },
+    { key: "F", status: "" },
+    { key: "G", status: "" },
+    { key: "H", status: "" },
+    { key: "J", status: "" },
+    { key: "K", status: "" },
+    { key: "L", status: "" },
+  ],
+  [
+    { key: "Z", status: "" },
+    { key: "X", status: "" },
+    { key: "C", status: "" },
+    { key: "V", status: "" },
+    { key: "B", status: "" },
+    { key: "N", status: "" },
+    { key: "M", status: "" },
+  ],
+];
